324978f10bb9e6a20ce4630ee7a819e17426fd15
Jia Yi <yijia2413@gmail.com>
2022-05-13 00:12:14 +0800

    Update

--- monitor.py
+++ monitor.py
@@ -7,18 +7,18 @@ from crypto import crypto
 g_dst_html = './result.html'
 
 def generate_html():
-
     l = []
-
     # 
     crypto_app = crypto.Crypto()
     crypto_html = crypto_app.get_html()
+    l.append(crypto_html)
 
     # 
     stock_app = stock.Stock()
     stock_html = stock_app.get_html()
+    l.append(stock_html)
 
-    l.extend(crypto_html, stock_html)
+    return l
 
 def dump_html(hl:list):
     with open(g_dst_html, 'w') as f:
